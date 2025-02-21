cbe5712b3a7b3a0a586a49be84b57997e03ab428
Jerry Xu <jerriven@gmail.com>
2025-02-20 19:14:34 -0800

    test

--- demo.py
+++ demo.py
@@ -8,6 +8,16 @@ current_file_path = os.path.abspath(__file__)
 current_dir = os.path.dirname(current_file_path)
 dataset_root = current_dir + "/dataset"
 
-model = YOLO("yolo11n.pt")
-results = model(dataset_root + "/images/test/6_00199_l.png")
-results[0].show()
+model = YOLO(current_dir + "/best.pt")
+# results = model.predict(dataset_root + "/images/test/6_00001_l.png")
+results = model.predict(source=dataset_root + "/images/test", save=True)
+
+
+# display the first result
+
+import matplotlib.pyplot as plt
+
+result_image = results[0].plot()
+plt.imshow(result_image)
+plt.axis("off")
+plt.show()
