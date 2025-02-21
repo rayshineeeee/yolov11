10a1bcfe41697a533a5d40eab7da0b08ae2a700a
raysun12142006@gmail.com <raysun@berkeley.edu>
2025-02-20 19:14:13 -0800

    demo complete, but doesn't work lol

--- demo.py
+++ demo.py
@@ -1,13 +1,17 @@
 from ultralytics import YOLO
 import os
-import torch
-import psutil
-
 
 current_file_path = os.path.abspath(__file__)
 current_dir = os.path.dirname(current_file_path)
 dataset_root = current_dir + "/dataset"
 
-model = YOLO("yolo11n.pt")
-results = model(dataset_root + "/images/test/6_00199_l.png")
-results[0].show()
+model = YOLO(current_dir+"/yolo11n_custom.pt")
+
+results = model(dataset_root + "/images/test/6_00199_l.png") 
+
+# # Access the results
+# for result in results:
+#     xy = result.masks.xy  # mask in polygon format
+#     xyn = result.masks.xyn  # normalized
+#     masks = result.masks.data  # mask in matrix format (num_objects x H x W)
+results[0].show()  # display to screen
